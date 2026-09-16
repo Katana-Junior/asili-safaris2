31c9d54ce350c97684ecae81d5d465a668bad707
John Katana <johnkatana624@gmail.com>
2026-09-16 14:53:06 +0300

    todays

--- src/App.js
+++ src/App.js
@@ -6,14 +6,9 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/upcomingevents" element={<UpcomingEvents />} />
           </Routes>
         </main>
       </div>
