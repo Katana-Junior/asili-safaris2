51567f28643c4dbbb17b80416713f9d86f15e4b4
John Katana <johnkatana624@gmail.com>
2026-09-16 14:00:00 +0300

    todays

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Header />
-      <Routes>
+        <Header />
         <main className="main-content">
-          
-        </Routes>
-          
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/upcomingevents" element={<UpcomingEvents />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
