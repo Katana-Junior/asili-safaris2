902edda9e8051d5c0af13b09f4368af1e0a70e58
John Katana <johnkatana624@gmail.com>
2026-09-20 22:07:30 +0300

    todays

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,18 @@
 import React from "react";
 import "./Pages.css";
-import "./About.css";
-import { Link } from "react-router-dom";
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-container">
+        <h1>Explore Beyond Infinity</h1>
+        <p>
+          Discover the beauty of Africa with Asili Safaris. We offer
+          unforgettable safari experiences that connect you with nature and
+          wildlife.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
