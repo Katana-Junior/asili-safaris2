56b715fde32470fba89abb753b2e41156b4db8c3
John Katana <johnkatana624@gmail.com>
2026-09-25 19:27:44 +0300

    todays

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,24 @@
 import React from "react";
-import "./Pages.css";
+import "./styles/Home.css";
+import logo from "../logos/asililogo.png";
+
 const Home = () => {
   return (
-    <div className="home">
-      <div className="home-container">
-        <h1>Explore Beyond Infinity</h1>
-        <p>
-          Discover the beauty of Africa with Asili Safaris. We offer
-          unforgettable safari experiences that connect you with nature and
-          wildlife.
-        </p>
+    <>
+      <div className="header">
+        <img src={logo} alt="Asili Safaris Logo" className="logo" />
       </div>
-    </div>
+      <div className="home-container">
+        <div className="hero-section">
+          <h1 className="slogan">Explore Beyond Infinity</h1>
+          <p className="home-description">
+            Discover the beauty of Africa with Asili Safaris. We offer
+            unforgettable safari experiences that connect you with nature and
+            wildlife.
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
